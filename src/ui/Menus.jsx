8408d2682fa63d530c4ee868835233d223a033a9
Young <18855490639@163.com>
2024-09-08 13:34:44 +0800

    0907

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -100,7 +100,7 @@ function Toggle({ id }) {
 }
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
   return createPortal(
