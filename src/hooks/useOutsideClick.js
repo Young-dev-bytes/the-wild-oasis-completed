4fd4edff440922963ed1f50e22d7f77d59f44155
Young <18855490639@163.com>
2024-09-08 13:21:17 +0800

    0908

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-export default function useOutsideClick(handler, listenCapturing = true) {
+export function useOutsideClick(handler, listenCapturing = true) {
   const ref = useRef();
 
   useEffect(() => {
