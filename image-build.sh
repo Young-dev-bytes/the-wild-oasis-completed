77bfd3db76dbe1785bd92565217de61d2a47f716
Young <18855490639@163.com>
2024-11-04 23:37:32 +0800

    1104

--- image-build.sh
+++ image-build.sh
@@ -1,4 +1,9 @@
 
-docker build -t younghub/the-wild-oasis-completed:0.0.1-SNAPSHOP .
+# # docker build -t younghub/the-wild-oasis-completed:0.0.1-SNAPSHOP .
 
-docker run -d -p 5173:80 younghub/the-wild-oasis-completed:0.0.1-SNAPSHOP
+# docker run -d -p 5173:80 younghub/the-wild-oasis-completed:0.0.1-SNAPSHOP
+
+VERSION=0.0.1-SNAPSHOP
+docker build -f Dockerfile -t the-wild-oasis-completed:$VERSION .
+docker tag the-wild-oasis-completed:$VERSION younghub/the-wild-oasis-completed:$VERSION
+docker push younghub/the-wild-oasis-completed:$VERSION
